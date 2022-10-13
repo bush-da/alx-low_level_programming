11e28ccfd52de77c700d339344c45f8a74906a2c
bush-da <danielsevani12@gmail.com>
2022-10-13 15:01:08 -0400

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the quote and that pices of
+ * Return: 1 if no error encountred
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	return (1);
+}
