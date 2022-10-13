b003f63b0b80116de967ea7bf49b53ee59c138a3
bush-da <danielsevani12@gmail.com>
2022-10-13 14:18:20 -0400

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,9 @@
  * Return: 0 if no error encountred
  */
 
-int main()
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar,"
+		"but the outcome is a piece of art,\n");
 	return (0);
 }
