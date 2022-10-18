c11ed52f83dfe6baea895d0356e043b1387653ec
bush-da <danielsevani12@gmail.com>
2022-10-18 11:57:19 -0400

    checks for lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,13 +3,14 @@
 /**
  * _islower - prints 1 if the character is lowercase
  * print 0 if the character is in uppercase
+ * c: is parameter takes single alphabet char
  * Return: 1 if lower 0 if its upper
  */
 
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
-		return 1;
+		return (1);
 	else
-		return 0;
+		return (0);
 }
