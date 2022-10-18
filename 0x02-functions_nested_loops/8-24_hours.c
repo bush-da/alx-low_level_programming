7586c8523982ffa8761e81036a62ac4e855a2495
bush-da <danielsevani12@gmail.com>
2022-10-18 15:07:37 -0400

    hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every min of a day
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	i = j = k = l = 0;
+
+	while (i <=2)
+	{
+		while (j <= 3)
+		{
+			while (k <= 5)
+			{
+				while (l <= 9)
+				{
+					_putchar('0' + i);
+					_putchar('0' + j);
+					_putchar(':');
+					_putchar('0' + k);
+					_putchar('0' + l);
+					_putchar('\n');
+					l++;
+				}
+				k++;
+				l = 0;
+			}
+			j++;
+			k = 0;
+			l = k;
+		}
+		i++;
+		k = 0;
+		j = 0;
+		l = 0;
+	}
+}
