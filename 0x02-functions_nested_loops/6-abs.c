ba54b073342894b438ccb18e1bd0ac32fb2b63a2
bush-da <danielsevani12@gmail.com>
2022-10-18 12:46:54 -0400

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - put a number into absolute value
+ * @c: takes argument
+ * Return: number if not negative
+ */
+
+int _abs(int c)
+{
+	if (c < 0)
+		return (c*-1);
+	else
+		return (c);
+}
+
