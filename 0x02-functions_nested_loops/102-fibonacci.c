67558561494170ff562a926c5a4b968be90fa7ee
bush-da <danielsevani12@gmail.com>
2022-10-19 10:44:17 -0400

    first 50 fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -4,18 +4,18 @@
  * main - prints the first 50 fibonacci number
  * Return: 0 if there is no error
  */
-int main()
+int main(void)
 {
 	
 	int fib1, fib2, fib3, num, count;
 
-	fib1 = 0;
-	fib2 = 1;
+	fib1 = 1;
+	fib2 = 2;
 
 	count = 2;
 
 	num = 50;
-	printf("%d %d, ", fib1, fib2);
+	printf("%d, %d, ", fib1, fib2);
 
 	while (count <= num)
 	{
