1d0b1ca86670697f81df85b43b3d3988681105ab
bush-da <danielsevani12@gmail.com>
2022-10-20 05:24:57 -0400

    first 98 fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,18 +7,19 @@
 
 int main(void)
 {
-	int fib1, fib2, fib3, count, num;
+	unsigned long int fib1, fib2, fib3, count, num;
 
 	fib1 = 1;
 	fib2 = 2;
 	num = 98;
 	count = 2;
-	printf("%d, %d, ", fib1, fib2);
+	printf("%lu, %lu", fib1, fib2);
 	while (count <= num)
 	{
 		fib3 = fib1 + fib2;
 		count++;
-		printf("%d, ", fib3);
+		printf(", ");
+		printf("%lu", fib3);
 		fib1 = fib2;
 		fib2 = fib3;
 	}
