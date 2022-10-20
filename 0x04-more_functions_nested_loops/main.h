3876e6f75abcaf7d921cc8c2dc9086373164d23f
bush-da <danielsevani12@gmail.com>
2022-10-20 09:46:13 -0400

    print square

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,5 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
-
 int _putchar(char c); 
 int _isupper(int c);
 int _isdigit(int c);
@@ -10,4 +9,5 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
+void print_square(int size);
 #endif
