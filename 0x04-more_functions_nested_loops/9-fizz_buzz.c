0df9797cf275ea430febdc30555bf66afc632b33
bush-da <danielsevani12@gmail.com>
2022-10-20 10:06:04 -0400

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - prints fizz buzz
+ * Return: 0 if there is no error
+ */
+
+int main()
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0 ) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
+			continue;
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+			continue;
+		}
+		else if (i % 5 == 0)
+		{
+			if (i == 100)
+				printf("Buzz");
+			else
+				printf("Buzz ");
+			continue;
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+
+	return (0);
+}
