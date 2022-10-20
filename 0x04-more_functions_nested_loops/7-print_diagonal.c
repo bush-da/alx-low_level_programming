cab7a2f146a00011b830cee4b4cc0628d96fb0ba
bush-da <danielsevani12@gmail.com>
2022-10-20 07:43:22 -0400

    draws a diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,12 +15,16 @@ void print_diagonal(int n)
 		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j <= i; j++)
+			{
 				_putchar(' ');
+			}
 			_putchar('\\');
 			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 	else
+	{
 		_putchar('\n');
+	}
+
 }
