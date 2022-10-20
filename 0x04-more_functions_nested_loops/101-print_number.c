b7e4e9b4945bf5c54630b31fd56b563d622d4cd0
bush-da <danielsevani12@gmail.com>
2022-10-20 13:38:16 -0400

    print number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_number - prints numbers
+ * @n: accepts number input
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+		print(n / 10);
+	_putchar(n % 10 + '0');
+}
