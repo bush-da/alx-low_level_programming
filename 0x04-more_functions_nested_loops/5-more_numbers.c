94ae0dab24792ea4da1be5bb56be14a135a35d86
bush-da <danielsevani12@gmail.com>
2022-10-20 06:52:41 -0400

    prints numbers 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the number base10
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 0 && j <= 9)
+				_putchar('0' + j);
+			else
+			{
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
