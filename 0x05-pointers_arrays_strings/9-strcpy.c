4adb73ffc779c862420c8c52c7ebb167d6461a45
bush-da <danielsevani12@gmail.com>
2022-10-24 11:48:08 -0400

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,10 +11,13 @@ char *_strcpy(char *dest, char *src)
 {
 	int len, i;
 
+	len = 0;
 	while (src[len] != '\0')
 		len++;
+
 	for (i = 0; i < len; i++)
 		dest[i] = src[i];
+
 	dest[len] = '\0';
 
 	return (dest);
