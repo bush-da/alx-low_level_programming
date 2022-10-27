3ccc5623fe3f36d45144528119b51faf239bdc05
bush-da <danielsevani12@gmail.com>
2022-10-27 08:13:33 -0400

    compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,22 +13,32 @@ int _strcmp(char *s1, char *s2)
 
 	len1 = len2 = 0;
 	while (s1[len1] != '\0')
+	{
 		len1++;
+	}
 	while (s2[len2] != '\0')
+	{
 		len2++;
+	}
 	if (len1 != len2)
 	{
 		if (len2 > len1)
+		{
 			return (s2[len2 - 1] -  48);
+		}
 		else
+		{
 			return (48 - s1[len1 - 1]);
+		}
 	}
 	else
 	{
 		for (i = len1 - 1; i >= 0; i--)
 		{
 			if (s1[i] == s2[i])
+			{
 				continue;
+			}
 			else
 			{
 				return ((s2[i] + '0') - (s1[i] + '0'));
@@ -38,4 +48,3 @@ int _strcmp(char *s1, char *s2)
 		return ((s1[i] + '0') - (s2[i] + '0'));
 	}
 }
-
