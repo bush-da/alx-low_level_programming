ca52ed3b0dfae6e96465a007f16fde97f8cfb222
bush-da <danielsevani12@gmail.com>
2022-10-27 06:22:49 -0400

    copy n byte of char

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,14 +10,11 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, len;
-	
-	len = 0;
-	while (dest[len] != '\0')
-		len++;
+	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-	dest[len - (i - 1)] = '\0';
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
