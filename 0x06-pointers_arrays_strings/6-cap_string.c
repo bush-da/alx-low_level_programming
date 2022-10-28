ae4d955c9ea4cd872585d79b37c91298348e04f1
bush-da <danielsevani12@gmail.com>
2022-10-28 10:06:02 -0400

    capitalize first letter

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,6 +11,9 @@ char *cap_string(char *c)
 	int i = 0;
 	int x= 0;
 	int key[] = {32, 9, 10, 44, 59, 33, 46, 34, 63, 40, 41, 123, 125};
+	if (c[i] >= 97 && c[i] <= 122)
+		c[i] = c[i] - 32;
+	i++;
 
 	while (c[i] != '\0')
 	{
@@ -21,10 +24,6 @@ char *cap_string(char *c)
 				if (c[i + 1] >= 97 && c[i + 1] <= 122)
 				{
 					c[i + 1] = c[i + 1] - 32;
-				}
-				if (c[i] == key[1])
-				{
-					c[i] = key[0];
 				}
 				break;
 			}
