4adc2fae1f2b585cdf73dfccb70d9289d54fbb94
bush-da <danielsevani12@gmail.com>
2022-11-04 08:13:09 -0400

    number of argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of argument
+ * @argc: number of argument
+ * @argv: argument passed to function
+ * Return: 0
+ */
+
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d", argc);
+	return (0);
+}
