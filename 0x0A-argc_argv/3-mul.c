694932245d3739fa40d8430bc6a6d0df9bdc5dd5
bush-da <danielsevani12@gmail.com>
2022-11-04 09:34:19 -0400

    multipli two number

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of argument
+ * @argv: arguments it receives
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int x, y;
+
+	if (argc <= 1)
+		printf("error");
+	else
+	{	
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+		printf("%d", x * y);
+	}
+	return (0);
+}
