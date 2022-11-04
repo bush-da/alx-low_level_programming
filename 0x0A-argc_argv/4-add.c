6442c2e4acf988b3a46978ee86e436e64e7a97a1
bush-da <danielsevani12@gmail.com>
2022-11-04 11:03:36 -0400

    add positive number

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive number
+ * @argc: number of argument
+ * @argv: arguments passed
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int tot, i;
+
+	tot = 0;
+	i = 1;
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	while (i < argc)
+	{
+		if (*argv[i] >= 48 && *argv[i] <= 58)
+		{
+			tot += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		i++;
+	}
+	printf("%d\n", tot);
+	return (0);
+}
