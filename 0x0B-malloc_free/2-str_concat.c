2ead163c6af423d4f6ad05ffc7cb24a600c58d4e
bush-da <danielsevani12@gmail.com>
2022-11-07 16:46:32 -0500

    concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,7 +18,8 @@ char *str_concat(char *s1, char *s2)
 		i++;
 	while (s2[j] != '\0')
 		j++;
-	a = malloc(i + j + 1);
+
+	a = malloc(sizeof(s1) + sizeof(s2));
 
 	i = j = 0;
 	while (s1[i] != '\0')
