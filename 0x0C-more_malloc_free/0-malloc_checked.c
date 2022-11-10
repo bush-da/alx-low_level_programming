0f0100c36f5e3dbc524605cb16c95b815f9cf7ff
bush-da <danielsevani12@gmail.com>
2022-11-10 11:30:37 -0500

    allocate memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - alocates memory using malloc
+ * @b: size of byte
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *x;
+	x = malloc(b);
+	if (x == NULL)
+	{
+		return ((void *)98);
+	}
+	return (x);
+}
