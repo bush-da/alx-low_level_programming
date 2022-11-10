a8fcf6cb7ecb9a979ef58abcd0eb26e5093644d5
bush-da <danielsevani12@gmail.com>
2022-11-10 15:16:11 -0500

    creates an array of integers

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,11 +12,18 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *a;
+	int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	a = malloc(nmemb * size);
 	if (a == NULL)
 		return (NULL);
+	i = 0;
+	while (i < nmemb * size)
+	{
+		a[i] = 0;
+		i++;
+	}
 	return (a);
 }
