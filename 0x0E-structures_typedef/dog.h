dd89e31b4744d74ce7dce8db67b5465acc3a9e87
bush-da <danielsevani12@gmail.com>
2022-11-14 13:56:33 -0500

    define new type

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+
+/**
+ * struct dog - new type define dog properties
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: the owner of dog
+ */
+
 struct dog
 {
 	char *name;
