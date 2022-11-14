f9a64e8b46ddcdae444f99d6a7ffefd6c0c1f309
bush-da <danielsevani12@gmail.com>
2022-11-14 14:15:09 -0500

    initialize a variable of type struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,9 +1,8 @@
-#include <stdlib.h>
 #include "dog.h"
 
 /**
  * init_dog - initialize a variable type struct dog
- * dog d - address of varible type struct dog
+ * d - address of varible type struct dog
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of a dog
@@ -11,9 +10,6 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-
-	d = malloc(sizeof(struct dog));
-
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
