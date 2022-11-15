131013d7e6b02b79c3fa526207629ff03b9fbb8a
bush-da <danielsevani12@gmail.com>
2022-11-15 11:40:02 -0500

    locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
  * _strchr - locates a character in a string
@@ -16,5 +15,5 @@ char *_strchr(char *s, char c)
 			return (s);
 		s = s + 1;
 	}
-	return (NULL);
+	return;
 }
