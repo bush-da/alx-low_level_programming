dbb8e4366615b0dbe97a5fa31544e9eca0050136
bush-da <danielsevani12@gmail.com>
2022-11-17 07:08:41 -0500

    prints strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,19 +13,21 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list ag;
 	char *arg;
 
-	i = 1;
 	va_start(ag, n);
+	i = 1;
 	while (i <= n)
 	{
 		arg = va_arg(ag, char *);
+
 		if (arg)
 			printf("%s", arg);
 		else
 			printf("(nil)");
+
 		if (separator && i < n)
 			printf("%s", separator);
 		i++;
 	}
-	va_end(ag);
 	printf("\n");
+	va_end(ag);
 }
