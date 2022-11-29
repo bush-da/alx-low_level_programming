58be22fed424cc54f0982c8a7f5533cb37e4161a
bush-da <danielsevani12@gmail.com>
2022-11-29 17:12:26 -0500

    delete node at nth

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes the node at index index
+ * @head: address of head
+ * @index: index of node
+ * Return: 1 if it succeeded -1 if it failed
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *prev;
+	listint_t *next;
+
+	prev = *head;
+	i = 0;
+	if (index != 0)
+	{
+		while (i < index -1 && prev != NULL)
+		{
+			prev = prev->next;
+			i++;
+		}
+	}
+
+	if (prev == NULL || (prev->next == NULL && index != 0))
+	{
+		return (-1);
+	}
+
+	next = prev->next;
+
+	if (index != 0)
+	{
+		prev->next = next->next;
+		free(next);
+	}
+	else
+	{
+		free(prev);
+		*head = next;
+	}
+
+	return (1);
+}
