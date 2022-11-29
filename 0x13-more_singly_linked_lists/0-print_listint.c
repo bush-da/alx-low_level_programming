39f881a605dd30d74ac8ad594a68ce50c9370d62
bush-da <danielsevani12@gmail.com>
2022-11-29 06:22:38 -0500

    print all the elements

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a lintint_t list
+ * @h: pointer to head
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int count;
+
+	count = 0;
+	if (h == NULL)
+		return (count);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+
+#endif
