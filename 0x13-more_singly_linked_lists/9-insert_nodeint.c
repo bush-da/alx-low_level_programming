737ddba86352f7d351c87bc86b5f1cb9b136d352
bush-da <danielsevani12@gmail.com>
2022-11-29 16:41:59 -0500

    insert node at nth

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		check++;
 	}
 
-	if (check != idx)
+	if (check != idx - 1)
 		return (NULL);
 
 	temp->next = tra->next;
