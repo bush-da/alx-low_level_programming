ca6402496ff29e7df682d3cd878489f39fd81b13
bush-da <danielsevani12@gmail.com>
2022-11-30 01:25:25 -0500

    delete node at nth

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -17,7 +17,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	i = 0;
 	if (index != 0)
 	{
-		while (i < index -1 && prev != NULL)
+		while (i < index - 1 && prev != NULL)
 		{
 			prev = prev->next;
 			i++;
