404f34c764c0ed335c0a331b3ed7158147f4a372
bush-da <danielsevani12@gmail.com>
2022-12-01 13:33:41 -0500

    binary to base 10

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: characters of binary number
+ * Return: unsigned int of binary number entered
+ */
+
+unsigned int power(unsigned int x);
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int sum;
+	unsigned int tot;
+	unsigned int pos;
+	int single;
+	const char *flag;
+
+	flag = b;
+	pos = 0;
+	tot = 0;
+	while (*b != '\0')
+		b++;
+	b--;
+	while (b >= flag)
+	{
+		single = *b - '0';
+		if (single != 0 && single != 1)
+			return (0);
+		sum = single * power(pos);
+		tot += sum;
+		pos++;
+		b--;
+	}
+	return (tot);
+}
+
+/**
+ * power - powers in 2 the power of input
+ * @x: input number
+ * Return: the result of power
+ */
+
+unsigned int power(unsigned int x)
+{
+	unsigned int tot;
+
+	tot = 1;
+	if (x == 0)
+		return (1);
+	while (x >= 1)
+	{
+		tot *= 2;
+		x--;
+	}
+	return (tot);
+}
