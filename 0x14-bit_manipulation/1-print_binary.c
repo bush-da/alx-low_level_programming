548dc26d6de14b43412992311230a4bc14aad2f9
bush-da <danielsevani12@gmail.com>
2022-12-01 16:14:27 -0500

    print in binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: base 10 number
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	int i;
+	int count;
+	unsigned long int current;
+
+	count = 0;
+	i = 63;
+	while (i >= 0)
+	{
+		current = n >> i;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
