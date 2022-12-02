c8b31a57ecf45551b1bff779ffa49eb88a2a2fdf
bush-da <danielsevani12@gmail.com>
2022-12-02 06:46:58 -0500

    remove ambiguity

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,6 +1,10 @@
 #include "main.h"
 
-
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: base 10 number
+ * Return: void
+ */
 
 void print_binary(unsigned long int n)
 {
@@ -8,21 +12,21 @@ void print_binary(unsigned long int n)
 
 	if (n == 0)
 	{
-		putchar('0');
+		_putchar('0');
 		return;
 	}
 	if (n == 1)
 	{
-		putchar('1');
+		_putchar('1');
 		return;
 	}
 	temp = n;
 	while (temp)
 	{
 		if (temp & n)
-			putchar('1');
+			_putchar('1');
 		else
-			putchar('0');
+			_putchar('0');
 		temp = temp >> 1;
 	}
 }
