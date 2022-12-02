e9ecce48416c504e2a700bce8e4ba77687f92f4a
bush-da <danielsevani12@gmail.com>
2022-12-02 04:58:32 -0500

    modify time complexity

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,27 +1,6 @@
 #include "main.h"
 
 /**
- * power - powers in 2 the power of input
- * @x: input number
- * Return: the result of power
- */
-
-unsigned int power(unsigned int x)
-{
-	unsigned int tot;
-
-	tot = 1;
-	if (x == 0)
-		return (1);
-	while (x >= 1)
-	{
-		tot *= 2;
-		x--;
-	}
-	return (tot);
-}
-
-/**
  * binary_to_uint - converts a binary number to an unsigned int
  * @b: characters of binary number
  * Return: unsigned int of binary number entered
@@ -29,17 +8,16 @@ unsigned int power(unsigned int x)
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int sum;
-	unsigned int tot;
-	unsigned int pos;
+	unsigned int sum, tot, pos;
 	int single;
 	const char *flag;
 
 	if (!b)
 		return (0);
+
 	flag = b;
-	pos = 0;
-	tot = 0;
+	pos = tot = 0;
+
 	while (*b != '\0')
 		b++;
 	b--;
@@ -48,7 +26,7 @@ unsigned int binary_to_uint(const char *b)
 		single = *b - '0';
 		if (single != 0 && single != 1)
 			return (0);
-		sum = single * power(pos);
+		sum = single << pos;
 		tot += sum;
 		pos++;
 		b--;
