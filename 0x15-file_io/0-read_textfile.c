3528a059ac5e0d8bb4f19c18b58bb4acc1db6f2c
bush-da <danielsevani12@gmail.com>
2022-12-05 15:06:34 -0500

    read text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,23 +9,33 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, status, i;
+	int fd;
+	ssize_t rcount, wcount;
+	char *buffer;
 
 	if (filename == NULL)
-		return (-1);
-	if (text_content == NULL)
-		return (1);
+		return (0);
 
-	fd = open(filename, O_APPEND | O_WRONLY);
+	fd = open(filename, O_RDWR);
 	if (fd == -1)
-		return (-1);
+		return (0);
 
-	for (i = 0; text_content[i] != '\0'; i++)
-		;
-	status = write(fd, text_content, i);
-	if (status == -1)
-		return (-1);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	rcount = read(fd, buffer, letters);
+	if (rcount == -1)
+		return (0);
+
+	wcount = write(STDOUT_FILENO, buffer, rcount);
+	if (wcount == -1 || rcount != wcount)
+		return (0);
+	free(buffer);
 
 	close(fd);
-	return (1);
+	return (wcount);
 }
