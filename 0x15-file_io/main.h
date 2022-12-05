47872f9c4b8ef2a4b9815ab7b1d7fd6bd43d4005
bush-da <danielsevani12@gmail.com>
2022-12-05 15:00:19 -0500

    read text file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,11 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 
