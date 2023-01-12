018b5adc8e4df67876cc6883514dea18aba7c761
bush-da <danielsevani12@gmail.com>
2023-01-12 15:50:30 -0500

    change varible name temp to newNode

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -23,7 +23,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	if (pointerToHead == NULL)
 	{
-		*head = temp;
+		*head = newNode;
 		return (*head);
 	}
 	while (pointerToHead->prev)
