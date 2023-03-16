6be1d9d9bcc358978eb02e4c0e68770200d51e25
bush-da <danielsevani12@gmail.com>
2023-03-16 10:39:10 -0400

    add condition to check it index + 1 is not accessible

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -24,6 +24,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		temp = temp->next;
 		count++;
 	}
+	if (count < idx)
+		return (NULL);
 
 	if (!temp)
 		return (add_dnodeint_end(h, n));
