4bc461e922c0ecce9c989235cd6baf8e07cb6486
bush-da <danielsevani12@gmail.com>
2023-03-16 07:32:05 -0400

    prints all the elements of a dlistint_t list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,18 +9,13 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 
-	size_t numNode;
-
-
-	numNode = 0;
-	if (h == NULL)
-		return numNode;
+	size_t numNode = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		h = h->next;
 		numNode++;
+		h = h->next;
 	}
 
 	return numNode;
