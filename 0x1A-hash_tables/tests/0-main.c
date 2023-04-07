04d22b19c27a5a64a3a9cb1c679624a640a6f393
bush-da <danielsevani12@gmail.com>
2023-04-07 07:18:23 -0700

    test case for created hash table

--- 0x1A-hash_tables/tests/0-main.c
+++ 0x1A-hash_tables/tests/0-main.c
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "hash_tables.h"
 
 /**
@@ -5,11 +8,13 @@
  *
  * Return: Always EXIT_SUCCESS.
  */
+
 int main(void)
 {
-    hash_table_t *ht;
+	hash_table_t *ht;
 
-    ht = hash_table_create(1024);
-    printf("%p\n", (void *)ht);
-    return (EXIT_SUCCESS);
+	ht = hash_table_create(1024);
+	printf("%p\n", (void *)ht);
+
+	return (EXIT_SUCCESS);
 }
