e9fa588fd29b435d06822cd9a87b7337bf0d98c2
bush-da <danielsevani12@gmail.com>
2023-04-07 12:15:58 -0700

    create a hash table and initialize them with NULL

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,12 +8,17 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = malloc(sizeof(hash_table_t));
+	hash_table_t *table;
 	unsigned long int i;
 
-	table->size = size;
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
 
-	table->array = malloc(table->size * sizeof(hash_node_t *));
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+		return (NULL);
 
 	i = 0;
 	while (i < table->size)
