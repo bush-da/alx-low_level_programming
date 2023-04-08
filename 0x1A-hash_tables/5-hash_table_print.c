11803a62521f78be721b3b2b3e2c79cab019e045
bush-da <danielsevani12@gmail.com>
2023-04-08 09:11:07 -0700

    print all elements of hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,41 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print hash table with key and value pair
+ * @ht: hash table
+ * Return: void
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	int flagForComma;
+	unsigned long int i;
+
+	flagForComma = 0;
+
+
+	if (ht == NULL)
+		return;
+
+
+	i = 0;
+	printf("{");
+	while (i < ht->size)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (flagForComma == 1)
+				printf(", ");
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+			}
+			flagForComma = 1;
+		}
+		i++;
+	}
+	printf("}\n");
+}
