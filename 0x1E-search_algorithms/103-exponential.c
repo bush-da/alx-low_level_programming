812241dda0731aa4c77354c6d540e89a116fee49
Daniel Tujuma <danielsevani12@gmail.com>
2024-07-20 18:18:17 -0400

    fix minor issue

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -68,6 +68,8 @@ int exponential_search(int *array, size_t size, int value)
 	if (array == NULL || size == 0)
 		return (-1);
 	expon = 1;
+	if (array[0] == value)
+		return (0);
 
 	while (expon <= size)
 	{
